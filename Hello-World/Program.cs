b1108a620002a1bf74a1182a7cb753bbdfeaeab1
Julian Hammer <jh@moinjulian.com>
2024-02-16 13:10:15 +0100

    Calcualte with Variables

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -9,11 +9,19 @@ namespace HelloWelt
             string name = "Herr Meier";
             string date = "16/02/2024";
 
+            int amount = 3;
+            double price = 7.95;
+            double total = amount * price;
+
             Console.WriteLine("Hallo "+ name + @",
 Hiermit übersende ich Ihnen die Rechnung für meine Leistung am " + date + @" Vielen Dank, dass Sie unsere Diesnste genutzt haben.
 
 Mit freundlichen Grüßen,
 Julian Hammer
+
+Menge: " + amount + @"
+Preis: " + price + @"
+Gesamt: " + total+ @"
  ");
 
         }
