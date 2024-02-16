c3995487159bb414028520ca147009f62207a792
Julian Hammer <jh@moinjulian.com>
2024-02-16 13:03:40 +0100

    Variales

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace HelloWelt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Herr Meier";
+            string date = "16/02/2024";
+
+            Console.WriteLine("Hallo "+ name + @",
+Hiermit übersende ich Ihnen die Rechnung für meine Leistung am " + date + @" Vielen Dank, dass Sie unsere Diesnste genutzt haben.
+
+Mit freundlichen Grüßen,
+Julian Hammer
+ ");
+
+        }
+    }
+}
